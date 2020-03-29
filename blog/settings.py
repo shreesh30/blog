68f89cf4744db64945be960150a45117dd2435cf
shreesh <shreesharya20@gmail.com>
2020-03-30 00:01:29 +0530

    settings changed

--- blog/settings.py
+++ blog/settings.py
@@ -151,11 +151,3 @@ AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL=None
-DEFAULY_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Stprage'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-
-
-PUBLIC_MEDIA_LOCATION = 'photos'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'blog.storage_backends.PublicMediaStorage'

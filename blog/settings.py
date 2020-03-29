db5463b25bdf9d7cd099a99029a62cd1744e5d85
shreesh <shreesharya20@gmail.com>
2020-03-29 14:31:31 +0530

    setting secret key changed

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@9g53#!7vu-ee&##$^z=&*5i(z@)^^s5o@_omr1!g!_&3m285c'
+# SECRET_KEY = '@9g53#!7vu-ee&##$^z=&*5i(z@)^^s5o@_omr1!g!_&3m285c'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

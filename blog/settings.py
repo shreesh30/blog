3afec03231903ac9335be6dfc11d9800f31c5d13
shreesh <shreesharya20@gmail.com>
2020-03-30 04:20:22 +0530

    setting changes for media function

--- blog/settings.py
+++ blog/settings.py
@@ -140,7 +140,7 @@ MESSAGE_TAGS={
 }
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL='https://django-technobuzz-files.s3.ap-south-1.amazonaws.com/photos/'
+MEDIA_URL=f'{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 
 django_heroku.settings(locals())
 
@@ -148,7 +148,9 @@ django_heroku.settings(locals())
 AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN=f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+PUBLIC_MEDIA_LOCATION='media'
 AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL=None
 
